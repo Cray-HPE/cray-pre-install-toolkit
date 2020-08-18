173a3c007f24d37fa16d3235e364ba361d9eb40d
Russell Bunch <rbunch@cray.com>
2020-08-18 03:14:43 -0500

    MTL-1043 Add cloudinit kernel param and fixup daemon
    Daemon now passes release/debug env var in. Mounts are also fixed up for /var/www/basecamp/config and ./static

--- suse/x86_64/cray-sles15sp2-JeOS/root/root/bin/basecamp-init.sh
+++ suse/x86_64/cray-sles15sp2-JeOS/root/root/bin/basecamp-init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -x
 if [ $# -lt 2 ]; then
     echo >&2 "usage: basecamp-init PIDFILE CIDFILE [CONTAINER [VOLUME]]"
     exit 1
@@ -11,16 +12,16 @@ BASECAMP_CONTAINER_NAME="${3-basecamp}"
 BASECAMP_CONTAINER_IMAGE="dtr.dev.cray.com/metal/mtl-${BASECAMP_CONTAINER_NAME}"
 BASECAMP_VOLUME_NAME="${4:-${BASECAMP_CONTAINER_NAME}-configs}"
 
-BASECAMP_VOLUME_MOUNT="/var/basecamp/configs:rw,exec"
+BASECAMP_VOLUME_MOUNT_CONFIG="/var/www/basecamp/config:/app/config:rw,exec"
+BASECAMP_VOLUME_MOUNT_STATIC="/var/www/basecamp/static:/app/static:rw,exec"
 
 command -v podman >/dev/null 2>&1 || { echo >&2 "${0##*/}: command not found: podman"; exit 1; }
 
-set -x
 
 # always ensure pid file is fresh
 rm -f "$BASECAMP_PIDFILE"
-mkdir -pv /var/basecamp/configs/
-
+mkdir -pv "$(echo ${BASECAMP_VOLUME_MOUNT_CONFIG} | cut -f 1 -d :)"
+mkdir -pv "$(echo ${BASECAMP_VOLUME_MOUNT_STATIC} | cut -f 1 -d :)"
 # Create basecamp container
 if ! podman inspect "$BASECAMP_CONTAINER_NAME" ; then
     rm -f "$BASECAMP_CIDFILE" || exit
@@ -31,8 +32,10 @@ if ! podman inspect "$BASECAMP_CONTAINER_NAME" ; then
         --cgroups=no-conmon \
         -d \
         --net host \
-        --volume "${BASECAMP_VOLUME_NAME}:${BASECAMP_VOLUME_MOUNT}" \
+        --volume "$BASECAMP_VOLUME_MOUNT_CONFIG" \
+        --volume "$BASECAMP_VOLUME_MOUNT_STATIC" \
         --name "$BASECAMP_CONTAINER_NAME" \
+        --env GIN_MODE="${GIN_MODOE:-release}" \
         "$BASECAMP_CONTAINER_IMAGE" || exit
     podman inspect "$BASECAMP_CONTAINER_NAME" || exit
 fi
