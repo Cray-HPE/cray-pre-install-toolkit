5acae9e20cd6d0239437f4780425b9e0ea4a144e
Sam Watters <watters.sam@gmail.com>
2020-09-04 20:53:33 -0500

    Add /etc/pit-release to LiveCD image
    Add the /etc/pit-release to LiveCD image to identify the
    code version, build date/time, and git commit hash.
    
    Remove the img-rename.sh script and move that operation
    into the build.sh script.

--- docker-build.sh
+++ docker-build.sh
@@ -7,10 +7,11 @@
 
 
 save_build_dir () {
+	echo "Build failure detected, copying build root from container to build_output."
 	if [[ -e /build && -e build_output ]]; then
 		cp -a /build/* build_output/
 	else
-		Echo "Failed before /build and ./build_output were created!"
+		echo "Failed before /build and ./build_output were created!"
 	fi
 }
 
@@ -19,6 +20,7 @@ trap save_build_dir ERR
 set -ex
 
 DESC_DIR=suse/x86_64/sles15sp2
+RELEASE_FILE=$DESC_DIR/root/etc/pit-release
 
 cd /base
 
@@ -38,8 +40,19 @@ else
     mkdir -p build_output
 fi
 
+# Write the pre-install-toolkit version file for build into root overlay
+cat << EOF > $RELEASE_FILE
+VERSION=$PIT_VERSION
+TIMESTAMP=$PIT_TIMESTAMP
+HASH=$PIT_HASH
+EOF
+
+
 # Build OS image tarball
 time /usr/bin/kiwi-ng --type iso --debug system build --description $DESC_DIR --target-dir /build
+
+# Delete the pre-install-toolkit version file for build from root overlay
+rm $RELEASE_FILE
 
 # Copy build artifacts to external mounted directory
 cp /build/*.iso /build/*.packages /build/*.verified ./build_output
