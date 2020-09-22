8c49793d3a125b3a9f582c05380ce7bdf9e9983d
Russell Bunch <rbunch@cray.com>
2020-09-22 15:21:21 -0500

    Allow any image selection

--- suse/x86_64/shasta-pre-install-toolkit-sle15sp2/root/root/bin/set-sqfs-links.sh
+++ suse/x86_64/shasta-pre-install-toolkit-sle15sp2/root/root/bin/set-sqfs-links.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-WEB_ROOT=/var/www/
-ln -snf "$(ls -1tr $WEB_ROOT/ephemeral/*initrd*xz | head -n 1)" "$WEB_ROOT/initrd.img.xz"
-ln -snf "$(ls -1tr $WEB_ROOT/ephemeral/*.kernel | head -n 1)" "$WEB_ROOT/kernel"
-ln -snf "$(ls -1tr $WEB_ROOT/ephemeral/k8s/*.squashfs | head -n 1)" "$WEB_ROOT/k8s-filesystem.squashfs"
-ln -snf "$(ls -1tr $WEB_ROOT/ephemeral/ceph/*.squashfs | head -n 1)" "$WEB_ROOT/ceph-filesystem.squashfs"
-ln -snf "$WEB_ROOT/ceph-filesystem.squashfs" "$WEB_ROOT/filesystem.squashfs"
+WEB_ROOT=/var/www
+image=${1:-ceph}
+ln -snf "$(ls -1tr $WEB_ROOT/ephemeral/data/*initrd*xz | head -n 1)" "$WEB_ROOT/initrd.img.xz"
+ln -snf "$(ls -1tr $WEB_ROOT/ephemeral/data/*.kernel | head -n 1)" "$WEB_ROOT/kernel"
+ln -snf "$(ls -1tr $WEB_ROOT/ephemeral/data/$image/*.squashfs | head -n 1)" "$WEB_ROOT/filesystem.squashfs"
+ln -snf "$(ls -1tr $WEB_ROOT/ephemeral/data/k8s/*.squashfs | head -n 1)" "$WEB_ROOT/k8s-filesystem.squashfs"
+ln -snf "$(ls -1tr $WEB_ROOT/ephemeral/data/ceph/*.squashfs | head -n 1)" "$WEB_ROOT/ceph-filesystem.squashfs"
+if [[ -z $1 ]]; then
+    echo "To set the active boot image, pass in any dir name in ${WEB_ROOT}/ephemeral/data."
+    echo "By default this sets ceph to boot first, check ${WEB_ROOT} file listing."
+fi
 test -e "$WEB_ROOT/initrd.img.xz" || echo No initrd
 test -e "$WEB_ROOT/kernel" || echo No Kernel
 test -e "$WEB_ROOT/filesystem.squashfs" || echo No SquashFS image found.
-echo "By default this sets ceph to boot first, check ${WEB_ROOT} file listing."
