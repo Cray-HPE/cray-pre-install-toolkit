5c9f8e688ada183eb1dcc383705a48793318c98a
John Heemstra <john.heemstra.jr@hpe.com>
2021-05-20 05:39:46 -0500

    Merge pull request #190 in MTL/cray-pre-install-toolkit from restore-bmcs-when-running-set-sqfs-links to main
    Squashed commit of the following:
    
    commit 7ac075e2e39db7ed433b5573c3f9712f045b071a
    Author: Russell Bunch <rbunch@cray.com>
    Date:   Wed May 19 16:01:10 2021 -0500
    
        New set-sqfs-links.sh with BMC dhcp restore baked in

--- suse/x86_64/cray-pre-install-toolkit-sle15sp2/root/root/bin/set-sqfs-links.sh
+++ suse/x86_64/cray-pre-install-toolkit-sle15sp2/root/root/bin/set-sqfs-links.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 WEB_ROOT=/var/www
 
+function call_bmc {
+    echo 'Attempting to set all known BMCs (from /etc/conman.conf) to dhcp mode'
+    echo "current BMC count: $(grep mgmt /var/lib/misc/dnsmasq.leases | wc -l)"
+    (
+    export username=root
+    export IPMI_PASSWORD=initial0
+    grep mgmt /etc/conman.conf | grep -v m001 | awk '{print $3}' | cut -d ':' -f2 | tr -d \" | xargs -t -i ipmitool -I lanplus -U $username -E -H {} lan set 3 ipsrc dhcp
+    ) >/var/log/metal-bmc-restore.$$.out 2>&1
+    echo "new BMC count: $(grep mgmt /var/lib/misc/dnsmasq.leases | wc -l)"
+}
+
 # Finds latest of each artifact regardless of subdirectory.
 k8s_initrd="$(find ${WEB_ROOT}/ephemeral/data/k8s -name *initrd* -printf '%T@ %p\n' | sort -n | tail -1 |  cut -f2- -d" ")"
 k8s_kernel="$(find ${WEB_ROOT}/ephemeral/data/k8s -name *.kernel -printf '%T@ %p\n' | sort -n | tail -1 |  cut -f2- -d" ")"
@@ -13,6 +24,8 @@ ceph_squashfs="$(find ${WEB_ROOT}/ephemeral/data/ceph -name *.squashfs -printf '
 if [[ "$(basename ${k8s_kernel} | cut -d '-' -f1,2)" != "$(basename ${ceph_kernel} | cut -d '-' -f1,2)" ]]; then
     echo 'Mismatching kernels! The discovered artifacts will deploy an undesirable stack.' >&2
 fi
+
+call_bmc
 
 echo "$0 is creating boot directories for each NCN with a BMC that has a lease in /var/lib/misc/dnsmasq.leases"
 echo "Nodes without boot directories will still boot the non-destructive iPXE binary."
