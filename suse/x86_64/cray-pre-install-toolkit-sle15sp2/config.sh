ce601b0acf7f049307631de1d8060966a1811008
John Heemstra <john.heemstra.jr@hpe.com>
2021-05-21 13:39:25 -0500

    Merge pull request #191 in MTL/cray-pre-install-toolkit from CASMINST-2318 to main
    Squashed commit of the following:
    
    commit b880961b6aed5bcfec7fb6cd9d777f9f435572ce
    Author: Brad Klein <bklein@cray.com>
    Date:   Fri May 21 10:53:29 2021 -0600
    
        Bump version of kubectl that gets installed on the pit to match 1.5 (1.19.9)

--- suse/x86_64/cray-pre-install-toolkit-sle15sp2/config.sh
+++ suse/x86_64/cray-pre-install-toolkit-sle15sp2/config.sh
@@ -116,7 +116,7 @@ ln -s $GOSS_BASE/automated/ncn-storage-checks /usr/bin/ncn-storage-checks
 #======================================
 # Install kubectl on LiveCD
 #--------------------------------------
-kubectl_version="1.18.6"
+kubectl_version="1.19.9"
 echo "Installing kubectl"
 curl -L https://storage.googleapis.com/kubernetes-release/release/v${kubectl_version}/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
 chmod a+x /usr/local/bin/kubectl
