b5c9442b505d4860fd74a164ad5a0d4abbd798f0
John Heemstra <heemstra@hpe.com>
2021-06-02 14:02:25 -0400

    Process new firmware package list

--- suse/x86_64/cray-pre-install-toolkit-sle15sp2/config.sh
+++ suse/x86_64/cray-pre-install-toolkit-sle15sp2/config.sh
@@ -51,6 +51,9 @@ install-packages /srv/cray/csm-rpms/packages/cray-pre-install-toolkit/base.packa
 echo "Installing packages from /srv/cray/csm-rpms/packages/cray-pre-install-toolkit/metal.packages"
 install-packages /srv/cray/csm-rpms/packages/cray-pre-install-toolkit/metal.packages
 
+echo "Installing packages from /srv/cray/csm-rpms/packages/cray-pre-install-toolkit/firmware.packages"
+install-packages /srv/cray/csm-rpms/packages/cray-pre-install-toolkit/firmware.packages
+
 #======================================
 # Setup baseproduct link
 #--------------------------------------
