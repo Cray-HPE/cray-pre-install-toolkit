25930aec3a7786985aae58b2651c1b6a96c02f7c
tantrieu-hpe <82524745+tantrieu-hpe@users.noreply.github.com>
2021-10-01 12:06:46 -0700

    Update config.sh
    fixed wrong case with curUrl loop variable.

--- suse/x86_64/cray-pre-install-toolkit-sle15sp3/config.sh
+++ suse/x86_64/cray-pre-install-toolkit-sle15sp3/config.sh
@@ -174,7 +174,7 @@ declare dataDir=${DATA_DIR:-/var/www/fw/river} \
         line= fileName= curUrl=
 printf -- "Downloading River BIOS, BMC, and CMC ... "
 mkdir -p ${dataDir}
-for cUrUrl in ${biosUrls}; do #{
+for curUrl in ${biosUrls}; do #{
   destFileName=${dataDir}/${curUrl##*/}
   curl "${curUrl}" >${destFileName} &
 done #}
