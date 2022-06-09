411ac23009e0eb913ad4c447c3d14e26118b46cf
Russell Bunch <doomslayer@hpe.com>
2022-06-09 12:19:32 -0500

    MTL-1796 Remove ad-hoc kubectl call in-favor of csm-rpms

--- suse/x86_64/cray-pre-install-toolkit-sle15sp3/config.sh
+++ suse/x86_64/cray-pre-install-toolkit-sle15sp3/config.sh
@@ -133,14 +133,6 @@ ln -s $GOSS_BASE/automated/ncn-kubernetes-checks /usr/bin/ncn-kubernetes-checks
 ln -s $GOSS_BASE/automated/ncn-storage-checks /usr/bin/ncn-storage-checks
 
 #======================================
-# Install kubectl on LiveCD
-#--------------------------------------
-kubectl_version="1.19.9"
-echo "Installing kubectl"
-curl -L https://storage.googleapis.com/kubernetes-release/release/v${kubectl_version}/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
-chmod a+x /usr/local/bin/kubectl
-
-#======================================
 # Firmware comes from HFP, but we can still keep these directories for convenience and backwards compatibility
 #--------------------------------------
 mkdir -pv /var/www/fw/river/hpe
