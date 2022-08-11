bed22828ccc6688e7fc905f4e5829c5bd06be9ac
Russell Bunch <doomslayer@hpe.com>
2022-08-11 13:34:19 -0500

    CASMINST-5026 Consistent Python Envs (#52)

--- suse/x86_64/cray-pre-install-toolkit-sle15sp4/config.sh
+++ suse/x86_64/cray-pre-install-toolkit-sle15sp4/config.sh
@@ -52,13 +52,6 @@ test -f /.profile && . /.profile
 echo "Configure image: [$kiwi_iname]..."
 
 #======================================
-# Add python symlink
-#--------------------------------------
-pushd /usr/bin
-ln -snf python3 python
-popd
-
-#======================================
 # Source rpm-functions...
 #--------------------------------------
 echo "Sourcing /srv/cray/csm-rpms/scripts/rpm-functions.sh"
@@ -80,6 +73,34 @@ install-packages /srv/cray/csm-rpms/packages/cray-pre-install-toolkit/base.packa
 
 echo "Installing packages from /srv/cray/csm-rpms/packages/cray-pre-install-toolkit/metal.packages"
 install-packages /srv/cray/csm-rpms/packages/cray-pre-install-toolkit/metal.packages
+
+#======================================
+# Setup Python3 environments.
+#--------------------------------------
+function setup_python {
+    local pythons
+
+    local        pip_ver='21.3.1'
+    local      build_ver='0.8.0'
+    local setuptools_ver='59.6.0'
+    local      wheel_ver='0.37.1'
+    local virtualenv_ver='20.15.1'
+
+    readarray -t pythons < <(find /usr/bin/ -regex '.*python3\.[0-9]+')
+    printf 'Discovered [%s] python binaries: %s\n' "${#pythons[@]}" "${pythons[*]}"
+    for python in "${pythons[@]}"; do
+        $python -m pip install -U "pip==$pip_ver" || $python -m ensurepip
+        $python -m pip install -U \
+            "build==$build_ver" \
+            "setuptools==$setuptools_ver" \
+            "virtualenv==$virtualenv_ver" \
+            "wheel==$wheel_ver" 
+    done
+}
+setup_python
+pushd /usr/bin
+ln -snf python3 python
+popd
 
 #======================================
 # Lock the kernel...
